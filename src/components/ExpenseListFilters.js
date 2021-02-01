7c9b9c4f48609b1ea9c13f3bed52765b66f48ab0
Shane O'Connor <spiderbaby@gmail.com>
2021-02-01 17:38:53 +0000

    Bugfix

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -71,7 +71,7 @@ const mapDispatchToProps = (dispatch) => ({
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
     setStartDate: (startDate) => dispatch(setStartDate(startDate)),
-    setEndDate: (startDate) => dispatch(setEndDate(endDate)) 
+    setEndDate: (endDate) => dispatch(setEndDate(endDate)) 
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
