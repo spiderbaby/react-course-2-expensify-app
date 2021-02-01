70b59ec970104f6ea7bb9ef5693e15f58d6cd403
Shane O'Connor <spiderbaby@gmail.com>
2021-02-01 17:40:21 +0000

    Removing test code

--- src/app.js
+++ src/app.js
@@ -15,32 +15,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({
-    description: 'Water bill',
-    amount: 4500,
-    note: 'Patrick never paid this'
-}));
-
-store.dispatch(addExpense({
-    description: 'Gas bill',
-    note: 'What a year',
-    createdAt: 1000
-}));
-
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 109500,
-    note: 'What a year'
-}));
-
-store.dispatch(sortByAmount());
-
-
-// setTextFilter -> bill (2 items) -> water
-// getVisibleExpenses -> print to screen
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
